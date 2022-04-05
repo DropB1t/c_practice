99bd6df806a1a2654650219ad3ae0b4433554473
Yuriy Rymarchuk <45283261+DropB1t@users.noreply.github.com>
2022-04-05 18:34:23 +0200

    L43

--- L40/prodcons-simple2.c
+++ L40/prodcons-simple2.c
@@ -10,17 +10,18 @@
 
 #include <util.h>
 
-#define MAX_STRING_LEN   1024
+#define MAX_STRING_LEN 1024
 
-typedef struct {
-  // risorsa condivisa (buffer di una sola posizione, memorizza una stringa)
-  char buffer[MAX_STRING_LEN]; 
-  char bufempty;       // flag che indica se il buffer e' vuoto o meno
+typedef struct
+{
+	// risorsa condivisa (buffer di una sola posizione, memorizza una stringa)
+	char buffer[MAX_STRING_LEN];
+	char bufempty; // flag che indica se il buffer e' vuoto o meno
 
-  // variabile di lock e di condizione per la sincronizzazione
-  // sul buffer condiviso, inizializzate nel main
-  pthread_mutex_t mutex;
-  pthread_cond_t  cond;
+	// variabile di lock e di condizione per la sincronizzazione
+	// sul buffer condiviso, inizializzate nel main
+	pthread_mutex_t mutex;
+	pthread_cond_t cond;
 } buffer_t;
 
 //
@@ -29,124 +30,142 @@ typedef struct {
 //
 
 // produttore
-void *Producer(void *arg) {
+void *Producer(void *arg)
+{
 
-  buffer_t *b = (buffer_t*)arg;
-  assert(b);
-  
-  for(;;) {
-  
-    LOCK(&b->mutex);
-    while(!b->bufempty)
-      WAIT(&b->cond, &b->mutex);
+	buffer_t *b = (buffer_t *)arg;
+	char localbuffer[MAX_STRING_LEN];
+	assert(b);
 
-    fprintf(stdout, "Inserisci una stringa (o 'exit'): ");
-    fflush(stdout);
-    if (fgets(b->buffer, MAX_STRING_LEN, stdin) == NULL ) {
-      perror("fgets");
-      return NULL;
-    }
-    if (strncmp(b->buffer, "exit", strlen("exit")) == 0) {
-      UNLOCK(&b->mutex);
-      break;
-    }
-    
-    b->buffer[strlen(b->buffer)-1]='\0';  // tolgo '\n'
-    b->bufempty = 0;
-    
-    SIGNAL(&b->cond);
-    UNLOCK(&b->mutex);
-  }
-  
-  // produco un valore speciale per provocare la terminazione
-  // del consumatore
-  LOCK(&b->mutex);
-  while(!b->bufempty)
-    WAIT(&b->cond,&b->mutex);
-  
-  strncpy(b->buffer, "\n", 2);  // suppongo che '\n' non sia mai nel buffer
-  b->bufempty = 0;
-  
-  SIGNAL(&b->cond);
-  UNLOCK(&b->mutex);
+	for (;;)
+	{
 
-  DBG("Producer [%ld] closing\n", pthread_self());  
-  return NULL;
+		fprintf(stdout, "Inserisci una stringa (o 'exit'): ");
+		fflush(stdout);
+		if (fgets(localbuffer, MAX_STRING_LEN, stdin) == NULL)
+		{
+			perror("fgets");
+			return NULL;
+		}
+		localbuffer[strlen(localbuffer) - 1] = '\0';
+
+		if (strncmp(localbuffer, "exit", strlen("exit")) == 0)
+		{
+			break;
+		}
+
+		//Sezione critica:
+		LOCK(&b->mutex);
+		while (!b->bufempty)
+			WAIT(&b->cond, &b->mutex);
+
+		strncpy(b->buffer, localbuffer, MAX_STRING_LEN);
+
+		// b->buffer[strlen(b->buffer) - 1] = '\0'; // tolgo '\n'
+		b->bufempty = 0;
+
+		SIGNAL(&b->cond);
+		UNLOCK(&b->mutex);
+	}
+
+	// produco un valore speciale per provocare la terminazione
+	// del consumatore
+	LOCK(&b->mutex);
+	while (!b->bufempty)
+		WAIT(&b->cond, &b->mutex);
+
+	strncpy(b->buffer, "\n", 2); // suppongo che '\n' non sia mai nel buffer
+	b->bufempty = 0;
+
+	SIGNAL(&b->cond);
+	UNLOCK(&b->mutex);
+
+	DBG("Producer [%ld] closing\n", pthread_self());
+	return NULL;
 }
 
-// consumatore 
-void *Consumer(void *arg) {
-  buffer_t *b = (buffer_t*)arg;
-  assert(b);
+// consumatore
+void *Consumer(void *arg)
+{
+	buffer_t *b = (buffer_t *)arg;
+	assert(b);
 
-  char mybuffer[MAX_STRING_LEN]; 
-  mybuffer[0]='\0';
+	char mybuffer[MAX_STRING_LEN];
+	mybuffer[0] = '\0';
 
-  while(1) {
-    LOCK(&b->mutex);
-    while(b->bufempty) 
-      WAIT(&b->cond,&b->mutex);
-    
-    if (b->buffer[0] == '\n') { // il produttore è terminato
-      UNLOCK(&b->mutex);
-      break;
-    }
+	while (1)
+	{
+		LOCK(&b->mutex);
+		while (b->bufempty)
+			WAIT(&b->cond, &b->mutex);
 
-    strncpy(mybuffer, b->buffer, MAX_STRING_LEN);
-    
-    b->bufempty = 1;
-    SIGNAL(&b->cond);
-    UNLOCK(&b->mutex);
+		if (b->buffer[0] == '\n')
+		{ // il produttore è terminato
+			UNLOCK(&b->mutex);
+			break;
+		}
 
-    fprintf(stdout, "Consumer, ricevuta stringa: %s\n", mybuffer);
-  }
-  
-  DBG("Consumer [%ld] closing\n", pthread_self());    
-  return NULL;
+		strncpy(mybuffer, b->buffer, MAX_STRING_LEN);
+
+		b->bufempty = 1;
+		SIGNAL(&b->cond);
+		UNLOCK(&b->mutex);
+
+		fprintf(stdout, "Consumer, ricevuta stringa: %s\n", mybuffer);
+		fflush(stdout);
+	}
+
+	DBG("Consumer [%ld] closing\n", pthread_self());
+	return NULL;
 }
 
-int main() {
-  pthread_t thconsumer, thproducer;
+int main()
+{
+	pthread_t thconsumer, thproducer;
 
-  
-  
-  buffer_t *b;
-  b = malloc(sizeof(buffer_t));
-  assert(b);
-  
-  b->bufempty=1;
-  b->buffer[0]='\0';
-  
-  if (pthread_mutex_init(&b->mutex, NULL) != 0) {
-    fprintf(stderr, "pthred_mutex_init failed\n");
-    return (EXIT_FAILURE);
-  }
-  if (pthread_cond_init(&b->cond, NULL) != 0) {
-    fprintf(stderr, "pthread_cond_init failed\n");
-    return (EXIT_FAILURE);
-  }
-  
-  if (pthread_create(&thconsumer, NULL, Consumer, b) != 0) {
-    fprintf(stderr, "pthread_create failed (Consumer)\n");
-    return (EXIT_FAILURE);
-  }
-  if (pthread_create(&thproducer, NULL, Producer, b) != 0) {
-    fprintf(stderr, "pthread_create failed (Producer)\n");
-    return (EXIT_FAILURE);
-  }    
-  if (pthread_join(thproducer, NULL) != 0) {
-    fprintf(stderr, "pthread_join failed (Producer)\n");
-    return (EXIT_FAILURE);
-  }
-  if (pthread_join(thconsumer, NULL) != 0) {
-    fprintf(stderr, "pthread_join failed (Consumer)\n");
-    return (EXIT_FAILURE);
-  }
+	buffer_t *b;
+	b = malloc(sizeof(buffer_t));
+	assert(b);
 
-  pthread_cond_destroy(&b->cond);
-  pthread_mutex_destroy(&b->mutex);
+	b->bufempty = 1;
+	b->buffer[0] = '\0';
 
-  free(b);
-  
-  return 0;
+	if (pthread_mutex_init(&b->mutex, NULL) != 0)
+	{
+		fprintf(stderr, "pthred_mutex_init failed\n");
+		return (EXIT_FAILURE);
+	}
+	if (pthread_cond_init(&b->cond, NULL) != 0)
+	{
+		fprintf(stderr, "pthread_cond_init failed\n");
+		return (EXIT_FAILURE);
+	}
+
+	if (pthread_create(&thconsumer, NULL, Consumer, b) != 0)
+	{
+		fprintf(stderr, "pthread_create failed (Consumer)\n");
+		return (EXIT_FAILURE);
+	}
+	if (pthread_create(&thproducer, NULL, Producer, b) != 0)
+	{
+		fprintf(stderr, "pthread_create failed (Producer)\n");
+		return (EXIT_FAILURE);
+	}
+	if (pthread_join(thproducer, NULL) != 0)
+	{
+		fprintf(stderr, "pthread_join failed (Producer)\n");
+		return (EXIT_FAILURE);
+	}
+	if (pthread_join(thconsumer, NULL) != 0)
+	{
+		fprintf(stderr, "pthread_join failed (Consumer)\n");
+		return (EXIT_FAILURE);
+	}
+
+	pthread_cond_destroy(&b->cond);
+	pthread_mutex_destroy(&b->mutex);
+
+	free(b);
+
+	return 0;
 }
---
+++ L43/pipeline.c
@@ -0,0 +1,187 @@
+#define  _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "utils/includes/util.h"
+#include "unboundedqueue.h"
+
+#define N_THREAD 3
+#define MAX_LINE_LEN 1024
+
+typedef struct
+{
+	int argc;
+	const char **argv;
+	Queue_t *q;
+} thread1_struct;
+
+typedef struct
+{
+	Queue_t *q1;
+	Queue_t *q2;
+} thread2_struct;
+
+void *TH1(void *arg)
+{
+	thread1_struct *th_struct = (thread1_struct *)arg;
+	assert(th_struct);
+
+	for (int i = 1; i < th_struct->argc; i++)
+	{
+		const char *filename = th_struct->argv[i];
+		int r;
+		size_t filesize;
+		if ((r = isRegular(filename, &filesize)) == 1)
+		{
+			/* if (filesize > MAX_FILE_SIZE)
+			{
+				fprintf(stderr, "Il file %s e' troppo grande, il limite e' %d byte\n", filename, MAX_FILE_SIZE);
+				continue;
+			} */
+			FILE *fp;
+			char *line = NULL;
+			size_t len = 0;
+			ssize_t read;
+
+			fp = fopen(filename, "r");
+			if (fp == NULL)
+				exit(EXIT_FAILURE);
+
+			while ((read = getline(&line, &len, fp)) != -1)
+			{
+				printf("Retrieved line of length %zu:\n", read);
+				printf("%s", line);
+				fflush(stdout);
+				char *msg = malloc(strlen(line)+1);
+				strcpy(msg,line);
+				push(th_struct->q, (void *)msg);
+			}
+
+			fclose(fp);
+			//if (line)
+				//free(line);
+		}
+		if (r == 0)
+			fprintf(stderr, "%s is not a regular file\n", filename);
+	}
+
+	char exit[7] = ">exit<";
+	push(th_struct->q, (void *)exit);
+	//free(line);
+
+	DBG("TH1 [%ld] closing\n", pthread_self());
+	return NULL;
+}
+
+void *TH2(void *arg)
+{
+	thread2_struct *th_struct = (thread2_struct *)arg;
+	assert(th_struct);
+
+	while (1)
+	{
+		char *line = pop(th_struct->q1);
+		if (strncmp(line, ">exit<", strlen(">exit<")) == 0)
+		{
+			char *msg = malloc(strlen(line)+1);
+			strcpy(msg,line);
+			push(th_struct->q2, (void *)msg);
+			//free(line);
+			break;
+		}
+
+		char *token;
+		token = strtok(line, " ");
+		if (token != NULL)
+		{
+			do
+			{
+				push(th_struct->q2, token);
+			} while ((token = strtok(NULL, " ")) != NULL);
+		}
+
+		//free(token);
+	}
+
+	char exit[7] = ">exit<";
+	push(th_struct->q2, (void *)exit);
+
+	DBG("TH2 [%ld] closing\n", pthread_self());
+	return NULL;
+}
+
+void *TH3(void *arg)
+{
+	Queue_t *q = (Queue_t *)arg;
+	assert(q);
+
+	while (1)
+	{
+		char *str = pop(q);
+		if (strncmp(str, ">exit<", strlen(">exit<")) == 0)
+		{
+			//free(str);
+			break;
+		}
+		fprintf(stdout, "Stringa -> %s\n",str);
+		fflush(stdout);
+		//free(str);
+	}
+
+	DBG("TH3 [%ld] closing\n", pthread_self());
+	return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+	pthread_t th[N_THREAD];
+
+	Queue_t *q1 = initQueue();
+	assert(q1);
+	Queue_t *q2 = initQueue();
+	assert(q2);
+
+	thread1_struct *th1_struct = malloc(sizeof(thread1_struct));
+	th1_struct->argc = argc;
+	th1_struct->argv = argv;
+	th1_struct->q = q1;
+
+	if (pthread_create(&th[0], NULL, TH1, th1_struct) != 0)
+	{
+		fprintf(stderr, "pthread_create failed (TH1)\n");
+		return (EXIT_FAILURE);
+	}
+
+	thread2_struct *th2_struct = malloc(sizeof(thread2_struct));
+	th2_struct->q1 = q1;
+	th2_struct->q2 = q2;
+
+	if (pthread_create(&th[1], NULL, TH2, th2_struct) != 0)
+	{
+		fprintf(stderr, "pthread_create failed (TH2)\n");
+		return (EXIT_FAILURE);
+	}
+
+	if (pthread_create(&th[2], NULL, TH3, q2) != 0)
+	{
+		fprintf(stderr, "pthread_create failed (TH2)\n");
+		return (EXIT_FAILURE);
+	}
+
+	for (size_t i = 0; i < N_THREAD; i++)
+	{
+		if (pthread_join(th[i], NULL) != 0)
+		{
+			fprintf(stderr, "pthread_join failed (Producer)\n");
+			return (EXIT_FAILURE);
+		}
+	}
+
+	deleteQueue(q1);
+	deleteQueue(q2);
+	free(th1_struct);
+	free(th2_struct);
+
+	return 0;
+}
