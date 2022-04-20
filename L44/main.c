717b2db88b1e97797867fab89100c7ca86c563dc
Yuriy Rymarchuk <45283261+DropB1t@users.noreply.github.com>
2022-04-20 16:17:20 +0200

    testing signals

--- L44/main.c
+++ L44/main.c
@@ -3,33 +3,28 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char const *argv[])
+#include "utils/includes/util.h"
+
+static void gestore(int signum)
+{
+    write(1,"Ricevuto SIGINT\n", 17);
+    _exit(EXIT_FAILURE);
+}
+
+int main(void)
 {
     struct sigaction s;
-    sigset_t set;
+    int i;
 
-    EC(sigfillset(&set), "fillset");
-    EC(pthread_sigmask(SIG_SETMASK, &set, NULL), "sigmask");
+    memset(&s, 0, sizeof(s));
+    s.sa_handler = gestore;
 
-    EC(sigaction(SIGINT, NULL, &s), "sigaction1");
-    s.sa_handler = handlerINT;
-    EC(sigaction(SIGINT, &s, NULL), "sigaction2");
-
-    s.sa_handler = handlerTSTP;
-    EC(sigaction(SIGTSTP, &s, NULL), "sigaction3");
-
-    s.sa_handler = handlerALRM;
-    EC(sigaction(SIGALRM, &s, NULL), "sigaction4");
-
-    sigdelset(&set, SIGINT);
-    sigdelset(&set, SIGALRM);
-    sigdelset(&set, SIGTSTP);
-
-    alarm(10);
-    while (1)
+    sigaction(SIGINT, &s, NULL);
+    for (i = 1;; i++)
     {
-        pause();
+        sleep(1);
+        printf("% d \n", i);
     }
 
-    return 0;
+    exit(EXIT_SUCCESS);
 }
